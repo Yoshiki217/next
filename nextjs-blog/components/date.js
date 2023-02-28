d3c054ee8192f1ec8c19f1044d4a203b3bbfc512
Yoshiki217 <effort0217@icloud.com>
2023-02-28 21:21:08 +0900

    add/title-styling

--- nextjs-blog/components/date.js
+++ nextjs-blog/components/date.js
@@ -2,7 +2,6 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  console.log(date)
   //return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
   return <date>{dateString}</date>
 }
